e7804010332db99fa4d05e86ceaadaecaa253fc1
kwierbol <k.a.wierbol@gmail.com>
2018-09-30 22:08:02 +0200

    correct availability evaluation

--- bookchecker.py
+++ bookchecker.py
@@ -9,6 +9,7 @@ pages = ['https://priam.umcs.lublin.pl:8443/lib/item?id=chamo:569809',
         ]
 
 library_name = "Biblioteka Główna UMCS"
+availability_flag = "Dostępne"
 
 for page in pages:
     
@@ -26,8 +27,8 @@ for page in pages:
     #assumption: the page exists so at least one 'odd' <tr> exists
     #parse NavigableString into unicode for further processing and to save memory (vide Beautiful Soup documentation) https://www.crummy.com/software/BeautifulSoup/bs4/doc/#navigablestring
     book_column = soup.find('tr', attrs={'class': 'odd'})
-    book_status = book_column.find(string="Dostępne")                            
+    book_status = book_column.find(string=availability_flag)                            
 
     #print out the availability of the book
-    if(book_status == "Dostępne"):
+    if(book_status == availability_flag):
         print ("AVAILABLE in "+library_name+":\t"+book_title)
